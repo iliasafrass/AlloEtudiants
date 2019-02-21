dd081d6ef5c2e749d2ee752283f14218c1bd926a
Ilias Afrass <ilias.afrass@etudiant.univ-lille1.fr>
2019-02-21 10:43:41 +0100

    fix button back for notification

--- AlloEtudiants/app/src/main/java/com/example/a707446/alloetudiant/notifications/NotificationFragment.java
+++ AlloEtudiants/app/src/main/java/com/example/a707446/alloetudiant/notifications/NotificationFragment.java
@@ -2,11 +2,14 @@ package com.example.a707446.alloetudiant.notifications;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
@@ -120,7 +123,6 @@ public class NotificationFragment extends AbstractFragment implements Notificati
     public void onResume() {
         super.onResume();
         disableItemsBottomBarChecked();
-
     }
 
     private void disableItemsBottomBarChecked(){
